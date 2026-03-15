737511c86ac90c1f1b41a87baa632f79cef092ff
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2026-03-15 12:20:45 +0900

    build: add devenv pre-commit hooks

--- devenv.nix
+++ devenv.nix
@@ -12,6 +12,32 @@
     };
   };
 
+  git-hooks.hooks = {
+    fix = {
+      enable = true;
+      name = "fix";
+      entry = "pnpm exec turbo run fix";
+      pass_filenames = false;
+      always_run = true;
+    };
+
+    typecheck = {
+      enable = true;
+      name = "typecheck";
+      entry = "pnpm exec turbo run typecheck";
+      pass_filenames = false;
+      always_run = true;
+    };
+
+    test = {
+      enable = true;
+      name = "test";
+      entry = "pnpm exec turbo run test";
+      pass_filenames = false;
+      always_run = true;
+    };
+  };
+
   packages = with pkgs; [
     bash-completion
     bashInteractive
